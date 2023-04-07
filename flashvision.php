adf765df0778627c0b3d7ccccd8fbc312720259f
collabre-code <117995168+collabre-code@users.noreply.github.com>
2023-04-07 16:34:02 -0400

    Update flashvision.php

--- flashvision.php
+++ flashvision.php
@@ -2,19 +2,7 @@
 
 
 
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-        <style>
-            table,th,td {
-                border: 1px solid black;
-            }
-        </style>
-    <body>
+
 <?php
 $server = "collabserver.database.windows.net";
 $database = "collabDataBase";
