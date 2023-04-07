61ffa638401e8e92bad5adb75b943a4bd0fbb8fb
collabre-code <117995168+collabre-code@users.noreply.github.com>
2023-04-07 16:29:58 -0400

    Update flashvision.php

--- flashvision.php
+++ flashvision.php
@@ -1,13 +1,52 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP with HTML Example</title>
-</head>
-<body>
-	<?php
-		$name = "John Doe";
-		echo "<h1>Hello, $name!</h1>";
-	?>
-</body>
-</html>
+<!DOCTYPE php>
+
+
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+        <style>
+            table,th,td {
+                border: 1px solid black;
+            }
+        </style>
+    <body>
+<?php
+$server = "collabserver.database.windows.net";
+$database = "collabDataBase";
+$username = "collabuser";
+$password = "YTc@3364";
+
+// Connect to the database
+$connection = mysqli_connect($server,$username,$password,$database);
+
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Perform a database query
+$query = "SELECT * FROM FlashVisionStatus";
+$result = mysqli_query($connection, $query);
+
+//$array = mysqli_fetch_array($result);
+
+// Check if the query was successful
+if (!$result) {
+    die("Query failed: " . mysqli_error($connection));
+}
+
+// Loop through the query results and display them
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "Location: " . $row["Abercrombi-Entry-1"] . "<br>";
+
+}
+
+// Close the database connection
+mysqli_close($connection);
+?>
 
