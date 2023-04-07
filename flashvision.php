fe4570dcd69b0b49acb0098732c544517244bc85
collabre-code <117995168+collabre-code@users.noreply.github.com>
2023-04-07 14:53:17 -0400

    Update flashvision.php

--- flashvision.php
+++ flashvision.php
@@ -1,74 +1,13 @@
 <!DOCTYPE html>
-
-
-
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-        <style>
-            table,th,td {
-                border: 1px solid black;
-            }
-        </style>
-    <body>
-
-       
-
-
-
-
-
-
-
-
-<center><h1>Flash Vision Status</h1></center>
-
-        
-<table>
-<tbody>
-
-<tr>
-<th>Abercrombie-Entry-1</th>
-<th>Abercrombie-Entry-2</th>
-<th>Abercrombie-Exit-1</th>
-<th>Abercrombie-Exit-2</th>
-<th>Abercrombie-Monthly-Express</th>
-<th>Egress-1</th>
-<th>Ingress-1</th>
-<th>Williams-St-entry-1</th>
-<th>Williams-St-Exit-1</th>
-<th>Williams-St-Monthly-Exit</th> 
-</tr>
-
-
-
-<tr>
-<td><?php echo $row['timeDelta']; ?></td>
-}
-<td><?php print_r($array[0]) ?></td>
-<td><?php print_r($array[0]) ?></td>
-<td><?php print_r($array[2]) ?></td>
-<td><?php print_r($array[3]) ?></td>
-<td><?php print_r($array[4]) ?></td>
-<td><?php print_r($array[5]) ?></td>
-<td><?php print_r($array[6]) ?></td>
-<td><?php print_r($array[7]) ?></td>
-<td><?php print_r($array[8]) ?></td>
-<td><?php print_r($array[9]) ?></td>
-</tr>
-}
-
-}
-
-
-</tbody>
-</table>
-
+<html>
+<head>
+	<title>PHP with HTML Example</title>
+</head>
+<body>
+	<?php
+		$name = "John Doe";
+		echo "<h1>Hello, $name!</h1>";
+	?>
 </body>
 </html>
-
 
